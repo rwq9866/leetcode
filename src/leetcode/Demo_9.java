5c8f6bc9af5cabf3d7edb087960617a7a9acafd1
muyou <841557177@qq.com>
2018-07-12 16:54:10 +0800

    9-回文数

---
+++ src/leetcode/Demo_9.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 回文数
+ * 
+ * @author muyou
+ *
+ */
+public class Demo_9 {
+	
+	// 将整数转换为字符串
+	public boolean isPalindrome(int x) {
+		return String.valueOf(x).equals(new StringBuilder(String.valueOf(x)).reverse().toString());
+	}
+	
+	// 不转换 的 情况下解决
+	public boolean isPalindrome2(int x) {
+		int n = x;
+		int m = 0;
+		if(x == Math.pow(2, 31) - 1) return false;
+		List<Integer> list = new ArrayList<>();
+		while(x > 0) {
+			list.add(x % 10);
+			x /= 10;
+		}
+		for(int i = 0;i < list.size();i++) m += list.get(i) * Math.pow(10, list.size() - i - 1);
+		return m == n;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Demo_9().isPalindrome(-121));
+		System.out.println(new Demo_9().isPalindrome2(2147483647));
+	}
+}
